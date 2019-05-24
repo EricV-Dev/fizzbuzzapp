052d55f5575969c03c28fffdcf8e399ac161abaf
EricV-Dev <eric_j_villarreal@Yahoo.com>
2019-05-23 21:06:22 -0500

    deleted unused function

--- src/app/runapp/runapp.component.ts
+++ src/app/runapp/runapp.component.ts
@@ -46,36 +46,3 @@ export class RunappComponent implements OnInit {
     }
   }
 }
-
-//   fizzBuzz() {
-//     let x = [];
-//     for (let i = 1; i <= 14; i++) {
-//       let retVal = "";
-//       let notDivBy3 = false;
-//       let notDivBy5 = false;
-
-//       if (i % 3 == 0) {
-//         retVal += "Fizz";
-//         notDivBy3 = true;
-//         x.push(retVal);
-//       }
-
-//       if (i % 5 == 0) {
-//         retVal += " Buzz";
-//         retVal = retVal.trim();
-//         notDivBy5 = true;
-//         x.push(retVal);
-//       }
-
-//       if (notDivBy3 == false && notDivBy5 == false) {
-//         x.push(i);
-//       }
-//     }
-//     return x;
-//   }
-
-//   outputFizzBuzz() {
-//     this.fizzResult = this.fizzBuzz();
-//     console.log(this.fizzResult);
-//   }
-//
