86a05943be40d3af261f2e12d2c1c965bbb68b7d
EricV-Dev <eric_j_villarreal@Yahoo.com>
2019-07-08 16:43:03 -0500

    test

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ const path = require("path");
 
 app.use(express.static(__dirname + "/dist"));
 
-app.all("*", (req, res) => {
-  res.status(200).sendFile(__dirname + "/dist/index.html");
+// app.all("*", (req, res) => {
+//   res.status(200).sendFile(__dirname + "/dist/index.html");
+// });
+
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname + "dist/index.html"));
 });
 
 app.listen(process.env.PORT || 8080);
