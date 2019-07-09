fce9f96b2a23a92a30f95ff1482325b9705b3a9f
EricV-Dev <eric_j_villarreal@Yahoo.com>
2019-07-08 20:35:21 -0500

    test

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-app.use(express.static(__dirname + "/dist"));
+app.use(express.static(__dirname + "/dist/FizzBuzzApp"));
 
 // app.all("*", (req, res) => {
 //   res.status(200).sendFile(__dirname + "/dist/index.html");
 // });
 
 app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname + "dist/index.html"));
+  res.sendFile(path.join(__dirname + "dist/FizzBuzzApp/index.html"));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000);
