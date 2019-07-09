06bbd5a72857225912f0272c2cfa7289bdf4bd52
EricV-Dev <eric_j_villarreal@Yahoo.com>
2019-07-08 20:52:06 -0500

    changed server

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-app.use(express.static(__dirname + "/dist/fizz-buzz-app"));
+app.use(express.static("./dist/{{fizzBuzzApp}}"));
 
 // app.all("*", (req, res) => {
 //   res.status(200).sendFile(__dirname + "/dist/index.html");
 // });
 
 app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname + "dist/fizz-buzz-app/index.html"));
+  res.sendFile(path.join(__dirname, "/dist/{{fizzBuzzApp}}/index.html"));
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 8080);
