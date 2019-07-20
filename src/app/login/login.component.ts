1f05ca7ac234b7858c444f222d57a9b47f7a0a6f
EricV-Dev <eric_j_villarreal@Yahoo.com>
2019-07-20 16:57:09 -0500

    2 way binding change

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { AuthService } from "../services/auth.service";
 import { Router } from "@angular/router";
-import { FormControl } from "@angular/forms";
 
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 
