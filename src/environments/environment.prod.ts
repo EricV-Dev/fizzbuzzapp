7e5d1dd05894c46846c4899630bd030b9507063a
EricV-Dev <eric_j_villarreal@Yahoo.com>
2019-09-08 17:10:27 -0500

    caps test displayuser

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   fizzURL: "https://still-river-42657.herokuapp.com/api/fizzBuzz",
   loginURL: "https://still-river-42657.herokuapp.com/api/login",
-  adminURL: "https://still-river-42657.herokuapp.com/api/displayUser",
+  adminURL: "https://still-river-42657.herokuapp.com/api/displayuser", // changed caps
   deleteURL: "https://still-river-42657.herokuapp.com/api/deleteUser",
   createUser: "https://still-river-42657.herokuapp.com/api/createUser",
   updateUser: "https://still-river-42657.herokuapp.com/api/updateUsername"
